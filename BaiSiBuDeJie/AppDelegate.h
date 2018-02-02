b3598a8bd3126a38339d69fb767d785d17d013c3
MrCat <13261137606@163.com>
2018-02-02 16:28:18 +0800

    1.0版本
    1.修复cocopods导入后，头文件索引问题
    2.修复JSONKit报错问题

---
+++ BaiSiBuDeJie/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  BaiSiBuDeJie
+//
+//  Created by MR董 on 2018/2/2.
+//  Copyright © 2018年 MRZD. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ BaiSiBuDeJie/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  BaiSiBuDeJie
+//
+//  Created by MR董 on 2018/2/2.
+//  Copyright © 2018年 MRZD. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
